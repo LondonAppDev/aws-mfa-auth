e069f5109c1161a308dd9acc6ae7f3d41ef11707
Mark Winterbottom <mark@londonappdeveloper.com>
2017-08-15 18:19:13 +0100

    Updated.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='aws-mfa-auth',
+    version='0.0.1',
+    description='Easily AWS MFA Authentication',
+    author='Mark Winterbottom',
+    author_email='mark@londonappdeveloper.com',
+    scripts=['aws-mfa-auth'],
+)
