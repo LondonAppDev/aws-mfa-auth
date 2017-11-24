6a1609d1b6af9a81be0512c970350b36fb5bba57
Mark Winterbottom <mark@londonappdeveloper.com>
2017-11-24 21:59:48 +0000

    Updated setup.

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import setup
+import os
+
+
+PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+def relative_path(path, base_dir=PACKAGE_ROOT):
+    return os.path.join(base_dir, path)
+
+
+def readme():
+    with open(relative_path('README.rst'), 'r') as f:
+        return f.read()
 
 
 setup(
@@ -9,4 +22,8 @@ setup(
     author_email='mark@londonappdeveloper.com',
     scripts=['aws-mfa-auth'],
     install_requires=['awscli'],
+    url='https://github.com/londonappdev/aws-mfa-auth',
+    long_description=readme(),
+    python_requires='>=3',
+    keywords='aws mfa authentication multi-factor auth'
 )
